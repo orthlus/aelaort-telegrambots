d276f57540bae91de823c0af79c87b1f27ac919d
ort <orthodoxplus@ya.ru>
2024-06-19 22:15:14 +0300

    client

--- src/main/java/art/aelaort/TelegramClientHelpers.java
+++ src/main/java/art/aelaort/TelegramClientHelpers.java
@@ -1,10 +1,20 @@
 package art.aelaort;
 
 import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 public class TelegramClientHelpers {
+	public static Message execute(SendMessage sendMessage, TelegramClient telegramClient) {
+		try {
+			return telegramClient.execute(sendMessage);
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static void execute(BotApiMethod<?> botApiMethod, TelegramClient telegramClient) {
 		try {
 			telegramClient.execute(botApiMethod);
