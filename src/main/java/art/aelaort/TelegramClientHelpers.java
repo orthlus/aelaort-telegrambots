6ea2c1037fb14c729336b56332aea57780a8e054
ort <orthodoxplus@ya.ru>
2024-07-08 20:36:13 +0300

    new helpers

--- src/main/java/art/aelaort/TelegramClientHelpers.java
+++ src/main/java/art/aelaort/TelegramClientHelpers.java
@@ -1,12 +1,47 @@
 package art.aelaort;
 
 import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 public class TelegramClientHelpers {
+	public static void execute(SendMediaGroup.SendMediaGroupBuilder<?, ?> sendMediaGroupBuilder, TelegramClient telegramClient) {
+		try {
+			telegramClient.execute(sendMediaGroupBuilder.build());
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void execute(SendPhoto.SendPhotoBuilder<?, ?> sendPhotoBuilder, TelegramClient telegramClient) {
+		try {
+			telegramClient.execute(sendPhotoBuilder.build());
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void execute(SendVideo.SendVideoBuilder<?, ?> sendVideoBuilder, TelegramClient telegramClient) {
+		try {
+			telegramClient.execute(sendVideoBuilder.build());
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void execute(SendMessage.SendMessageBuilder<?, ?> sendMessageBuilder, TelegramClient telegramClient) {
+		try {
+			telegramClient.execute(sendMessageBuilder.build());
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static Message execute(SendMessage sendMessage, TelegramClient telegramClient) {
 		try {
 			return telegramClient.execute(sendMessage);
