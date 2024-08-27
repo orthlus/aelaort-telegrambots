37011e4ca577e7c52c6b2bb00b44e4ee52ceb877
ort <orthodoxplus@ya.ru>
2024-08-27 18:30:30 +0300

    Command map - HashMap to TreeMap

--- src/main/java/art/aelaort/Command.java
+++ src/main/java/art/aelaort/Command.java
@@ -1,18 +1,24 @@
 package art.aelaort;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface Command {
 	String getCommand();
 
 	static <T extends Command> Map<String, T> buildMap(Class<T> commandsClass) {
 		if (commandsClass.isEnum()) {
-			Map<String, T> commandsMap = new HashMap<>();
-			for (T command : commandsClass.getEnumConstants())
-				commandsMap.put(command.getCommand(), command);
-			return commandsMap;
-
+			return Stream.of(commandsClass.getEnumConstants())
+					.sorted()
+					.collect(Collectors.toMap(
+							Command::getCommand,
+							Function.identity(),
+							(t, t2) -> t,
+							TreeMap::new
+					));
 		}
 		throw new RuntimeException("passed not enum class");
 	}
