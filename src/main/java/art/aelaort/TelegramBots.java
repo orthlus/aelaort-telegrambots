81908fe99b742522ef83581e41210984c85565f7
ort <orthodoxplus@ya.ru>
2024-09-08 19:07:19 +0300

    naming

--- src/main/java/art/aelaort/TelegramBots.java
+++ src/main/java/art/aelaort/TelegramBots.java
@@ -8,16 +8,16 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class TelegramBots {
-	public static TelegramBotsLongPollingApplication telegramBotsApplication() {
+	public static TelegramBotsLongPollingApplication application() {
 		return new TelegramBotsLongPollingApplication();
 	}
 
 	public static TelegramInit createTelegramInit(List<SpringLongPollingBot> bots, Supplier<TelegramUrl> telegramUrlSupplier) {
-		return new TelegramInit(telegramBotsApplication(), bots, telegramUrlSupplier);
+		return new TelegramInit(application(), bots, telegramUrlSupplier);
 	}
 
 	public static TelegramInit createTelegramInit(List<SpringLongPollingBot> bots) {
-		return new TelegramInit(telegramBotsApplication(), bots, () -> TelegramUrl.DEFAULT_URL);
+		return new TelegramInit(application(), bots, () -> TelegramUrl.DEFAULT_URL);
 	}
 
 	public static Supplier<TelegramUrl> telegramUrlSupplier(String url) {
