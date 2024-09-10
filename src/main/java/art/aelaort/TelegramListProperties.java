7e5df81d1ace69d6f29cbcd028b72c8d93f18fcd
ort <orthodoxplus@ya.ru>
2024-09-10 21:07:07 +0300

    list properties - add prefix

--- src/main/java/art/aelaort/TelegramListProperties.java
+++ src/main/java/art/aelaort/TelegramListProperties.java
@@ -2,9 +2,13 @@ package art.aelaort;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
+@ConfigurationProperties("telegram.list")
 public class TelegramListProperties {
 	private String url;
 	private String user;
